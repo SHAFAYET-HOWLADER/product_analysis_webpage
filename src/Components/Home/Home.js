84f4ae21c99763b5729115e35fe3a27eacc92b7d
unknown <you@example.com>
2022-04-03 23:55:09 +0600

    home components added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import banner from '../../images/banner.jpg'
+import './Home.css'
 const Home = () => {
     return (
-    <h1>this is home</h1>
+       <div className="container">
+            <div className='home'>
+             <div className='home_left'>
+                 <h1>A smartwatch is a wearable 
+                     <br /> computing device</h1>
+                 <p> In addition to telling time, many smartwatches are Bluetooth-capable. The watch becomes a wireless Bluetooth adaptor capable of extending the capabilities of the wearer's smartphone to the watch.</p>
+             </div>
+             <div className='home_right'>
+                 <img src={banner} alt="img" />
+             </div>
+        </div>
+       </div>
     );
 };
 
