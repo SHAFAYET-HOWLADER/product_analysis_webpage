3afbc5fd34a9f0a39e5493cd30ab1e84cd6dcda6
unknown <you@example.com>
2022-04-04 16:59:12 +0600

    contact page added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,31 @@
 import React from 'react';
-
+import contact_img from '../../images/contact_img.jpeg';
+import './Contact.css'
 const Contact = () => {
     return (
-        <div>
-            <h1>this is contact</h1>
+        <div className='container'>
+            <h1>Contact us</h1>
+            <div className='contact_area'>
+                <img src={contact_img} alt="img" />
+                <div className='contact_text'>
+                    <h2>Connected with us</h2>
+                    <h4> Email : watchMartBD2gmail.com</h4>
+                    <h4>Phone : +880 1890341583</h4>
+                    <h4> Address : Star Tech & Engineering Ltd
+                        <br />
+                        6th floor, 28 Kazi Nazrul Islam Ave,Navana Zohura</h4>
+                        <h2>Become a buyer</h2>
+                       <div className="input_area">
+                       <input type="text" placeholder='Email'/>
+                        <br />
+                        <input type="password"  placeholder='Password'/>
+                        <br />
+                        <button type='button'>Register</button>
+                       </div>
+                </div>
+            </div>
         </div>
     );
 };
-
 export default Contact;
+
